0f7217d1f712c2ffc86f14c95ef4cc196a57abc1
ywheel <wheel1989@gmail.com>
2015-02-14 01:03:04 +0800

    Format.

--- src/ywheel/leetcode/_001_two_sum/TwoSum.java
+++ src/ywheel/leetcode/_001_two_sum/TwoSum.java
@@ -18,40 +18,41 @@ import java.util.Comparator;
  * 
  */
 public class TwoSum {
-    public int[] twoSum(final int[] numbers, int target) {
-    	Integer[] index = new Integer[numbers.length];
-    	for (int i=0; i<numbers.length; i++) {
-    		index[i] = i;
-    	}
-    	Arrays.sort(index, new Comparator<Integer>() {
-    		public int compare(Integer o1, Integer o2) {
-    			return Integer.compare(numbers[o1], numbers[o2]);
-    		}
-    	});
-    	int[] result = new int[2];
-    	for (int i=0; i<numbers.length - 1; i++) {
-    		for (int j=i+1; j<numbers.length; j++) {
-    			int sum = numbers[index[i]] + numbers[index[j]];
-    			if (sum == target) {
-    				result[0] = (index[i] < index[j] ? index[i] : index[j]) + 1;
-    				result[1] = (index[i] > index[j] ? index[i] : index[j]) + 1;
-    				return result;
-    			} else if (sum > target) {
-    			    break;
-    			}
-    		}
-    	}
-        return null;
-    }
-    
-    public static void main(String[] args) {
-    	TwoSum solution = new TwoSum();
-    	int[] numbers = {2, 7, 11, 15};
-    	int target = 9;
-    	int[] results = solution.twoSum(numbers, target);
-    	for (int index=0; index<results.length; index++) {
-    		System.out.print("index" + (index + 1) + "=" + results[index] + " ");
-    	}
-    	
-    }
+	public int[] twoSum(final int[] numbers, int target) {
+		Integer[] index = new Integer[numbers.length];
+		for (int i = 0; i < numbers.length; i++) {
+			index[i] = i;
+		}
+		Arrays.sort(index, new Comparator<Integer>() {
+			public int compare(Integer o1, Integer o2) {
+				return Integer.compare(numbers[o1], numbers[o2]);
+			}
+		});
+		int[] result = new int[2];
+		for (int i = 0; i < numbers.length - 1; i++) {
+			for (int j = i + 1; j < numbers.length; j++) {
+				int sum = numbers[index[i]] + numbers[index[j]];
+				if (sum == target) {
+					result[0] = (index[i] < index[j] ? index[i] : index[j]) + 1;
+					result[1] = (index[i] > index[j] ? index[i] : index[j]) + 1;
+					return result;
+				} else if (sum > target) {
+					break;
+				}
+			}
+		}
+		return null;
+	}
+
+	public static void main(String[] args) {
+		TwoSum solution = new TwoSum();
+		int[] numbers = { 2, 7, 11, 15 };
+		int target = 9;
+		int[] results = solution.twoSum(numbers, target);
+		for (int index = 0; index < results.length; index++) {
+			System.out
+					.print("index" + (index + 1) + "=" + results[index] + " ");
+		}
+
+	}
 }
